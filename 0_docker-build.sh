ebbbd2062557483a053b844c503644394b652b5b
Greg McKeon <greg.mckeon@mongodb.com>
2019-11-12 08:31:25 -0800

    Add 4.2 testing.

--- 0_docker-build.sh
+++ 0_docker-build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-docker build -t mongo/mongodb-tests:3.6 .
+if [[ $1 = "3.6" ]]; then
+  docker build -f Dockerfile-3.6 -t mongo/mongodb-tests:3.6 .
+elif [[ $1 = "4.2" ]]; then
+  docker build -f Dockerfile-4.2 -t mongo/mongodb-tests:4.2 .
+else
+  echo "Please specify either version 3.6 or 4.2"
+fi
