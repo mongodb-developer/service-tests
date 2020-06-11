d277ffd42be2261b78b6e42c738ed8a75d5d6612
Vincent Mazot <vincent.mazot@mongodb.com>
2020-06-11 18:55:27 +0200

    parameterize dockerfile

--- 0_docker-build.sh
+++ 0_docker-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+#source config/${1}.cfg
 if [[ $1 = "3.6" ]]; then
-  docker build -f Dockerfile-3.6 -t mongo/mongodb-tests:3.6 .
+  branch=r3.6.18
+  mongoshell_package=debian92-3.6.18
+  docker build -f Dockerfile-$1 --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
 elif [[ $1 = "4.2" ]]; then
-  docker build -f Dockerfile-4.2 -t mongo/mongodb-tests:4.2 .
+  branch=r4.2.6
+  mongoshell_package=debian10-4.2.6
+  docker build -f Dockerfile-$1 --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
 else
   echo "Please specify either version 3.6 or 4.2"
 fi
