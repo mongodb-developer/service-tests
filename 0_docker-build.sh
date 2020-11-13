6f89490fde1e9d39cff2b13885c7f722f5607dc9
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-11-13 06:07:36 +0100

    Dockerfile-4.2 and Dockerfile-4.4 now identical so removing duplicate

--- 0_docker-build.sh
+++ 0_docker-build.sh
@@ -7,11 +7,11 @@ if [[ $1 = "4.0" ]]; then
 elif [[ $1 = "4.2" ]]; then
   branch=v4.2
   mongoshell_package=debian10-4.2.10
-  docker build -f Dockerfile-$1 --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
+  docker build -f Dockerfile --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
 elif [[ $1 = "4.4" ]]; then
   branch=v4.4
   mongoshell_package=debian10-4.4.1
-  docker build -f Dockerfile-$1 --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
+  docker build -f Dockerfile --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .
 else
   echo "Please specify either version 4.0, 4.2 or 4.4"
 fi
