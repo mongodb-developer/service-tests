5c07acf92d2262b9802c8c1f100d9aa77e081b38
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-11-13 06:00:20 +0100

    add all the failing tests in DBAAS

--- list-failing-tests.sh
+++ list-failing-tests.sh
@@ -3,6 +3,6 @@ echo "Failing tests"
 
 for f in $(find result* -name "*json"); do
   echo "==> $f"
-  cat "$f" | python3 -m json.tool | grep 'status": "fail' -A5 | grep test | sed 's/          "test_file": "/- /g' | sed 's/"//g'
+  cat "$f" | python3 -m json.tool | grep 'status": "fail' -A5 -B1 | grep test_file | sed 's/          "test_file": "/- /g' | sed 's/"//g'
   echo ""
 done
