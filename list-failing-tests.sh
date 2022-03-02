678aa338fbb0d370b766e822e204e5aac21d982f
Craig Homa <31703134+craigfhoma@users.noreply.github.com>
2022-03-02 16:38:50 -0500

    Delete list-failing-tests.sh

--- list-failing-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Failing tests"
-
-for f in $(find result* -name "*json"); do
-  echo "==> $f"
-  cat "$f" | python3 -m json.tool | grep 'status": "fail' -A5 -B1 | grep test_file | sed 's/          "test_file": "/- /g' | sed 's/[,"]//g'
-  echo ""
-done
