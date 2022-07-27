cbcf2770a611e4c806b16a1808c1d65bf9a0f0c0
Craig Homa <31703134+craigfhoma@users.noreply.github.com>
2022-07-27 13:05:04 -0400

    Update docker-build.sh

--- post-5.0/docker-build.sh
+++ post-5.0/docker-build.sh
@@ -15,7 +15,7 @@ elif [[ $1 = "5.2" ]]; then
   docker build -f Dockerfile --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .  
 elif [[ $1 = "6.0" ]]; then
   branch=v6.0
-  mongoshell_package=debian11-v6.0-latest
+  mongoshell_package=debian10-v6.0-latest
   docker build -f Dockerfile --build-arg version=$1 --build-arg branch=$branch --build-arg mongoshell_package=$mongoshell_package -t mongo/mongodb-tests:$1 .  
 else
   echo "Please specify either version 5.0, 5.1, 5.2, or 6.0. Please use the pre-5.0 directory for running older versions."
