63d48b67fbf4c65c4a405b970c4dc248cf54714e
Craig Homa <31703134+craigfhoma@users.noreply.github.com>
2023-01-05 14:27:05 -0500

    Update docker-run.sh

--- post-5.0/docker-run.sh
+++ post-5.0/docker-run.sh
@@ -22,6 +22,34 @@ docker logs mongodb-tests-decimal-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_decim
 docker rm -v mongodb-tests-decimal-${VERSION}
 echo "Decimal tests complete"
 
+echo "Starting test suite - Core"
+docker run --name mongodb-tests-core-${VERSION} -e "URI=${URI}" -v ${LOCAL_RESULTS_DIR}:/results ${IMAGE} core > /dev/null
+docker logs mongodb-tests-core-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_core.log
+docker rm -v mongodb-tests-core-${VERSION}
+echo "Core tests complete"
 
+echo "Starting test suite - Transactions"
+docker run --name mongodb-tests-core-txns-${VERSION} -e "URI=${URI}" -v ${LOCAL_RESULTS_DIR}:/results ${IMAGE} core_txns > /dev/null
+docker logs mongodb-tests-core-txns-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_core_txns.log
+docker rm -v mongodb-tests-core-txns-${VERSION}
+echo "Transactions tests complete"
+
+echo "Starting test suite - JSON Schema"
+docker run --name mongodb-tests-json-schema-${VERSION} -e "URI=${URI}" -v ${LOCAL_RESULTS_DIR}:/results ${IMAGE} json_schema > /dev/null
+docker logs mongodb-tests-json-schema-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_json_schema.log
+docker rm -v mongodb-tests-json-schema-${VERSION}
+echo "JSON Schema tests complete"
+
+echo "Starting test suite - Change Streams"
+docker run --name mongodb-tests-change-streams-${VERSION} -e "URI=${URI}" -v ${LOCAL_RESULTS_DIR}:/results ${IMAGE} change_streams > /dev/null
+docker logs mongodb-tests-change-streams-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_change_streams.log
+docker rm -v mongodb-tests-change-streams-${VERSION}
+echo "Change Streams tests complete"
+
+echo "Starting test suite - Aggregation"
+docker run --name mongodb-tests-aggregation-${VERSION} -e "URI=${URI}" -v ${LOCAL_RESULTS_DIR}:/results ${IMAGE} aggregation > /dev/null
+docker logs mongodb-tests-aggregation-${VERSION} > ${LOCAL_RESULTS_DIR}/stdout_aggregation.log
+docker rm -v mongodb-tests-aggregation-${VERSION}
+echo "Aggregation tests complete"
 
 echo "All tests complete"
