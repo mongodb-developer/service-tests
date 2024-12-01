1e325012f56ed8a00be17ecda4e7e091a67c98ba
rahumong2024 <150271225+rahumong2024@users.noreply.github.com>
2024-12-01 19:46:33 +0100

    Update config.py

--- Current Tests/Document DB Compatibility Tests/docdbcomp/config.py
+++ Current Tests/Document DB Compatibility Tests/docdbcomp/config.py
@@ -1,16 +1,13 @@
 # config.py
 # config.py
 
-# MongoDB Atlas cluster for test data (original MongoDB)
-#MONGODB_URI = 'mongodb+srv://rahulverma:09Ph2007@vcompatibilitytest.n0kts.mongodb.net/?retryWrites=true&w=majority&appName=vcompatibilitytest'
-#MONGODB_DB_NAME = 'testdb_mongodb'
 
 # Amazon DocumentDB connection
-DOCDB_URI = 'mongodb://rahulverma:09Ph2007@docdbcomp.cluster-cxjimx8ndms0.eu-west-3.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=/home/ubuntu/docdbcomp/global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
+DOCDB_URI = 'enter your Document DB connection string here'
 DOCDB_DB_NAME = 'testdb_documentdb'
 
 # MongoDB Atlas cluster for storing results
-RESULT_DB_URI = 'mongodb+srv://rahulverma:09Ph2007@vcompatibilitytest.n0kts.mongodb.net/?retryWrites=true&w=majority&appName=vcompatibilitytest'
+RESULT_DB_URI = 'enter your Atlas connection string where the results will be stored'
 RESULT_DB_NAME = 'docdb_web_prod'
 RESULT_COLLECTION_NAME = 'correctness'
 
@@ -29,5 +26,5 @@ KMS_PROVIDERS = {
 KEY_VAULT_NAMESPACE = 'encryption.__keyVault'
 
 # Paths to mongodump and mongorestore tools
-MONGODUMP_PATH = '/Users/rahul.verma/Desktop/vCore Compatibility Test Suite/mongodb-database-tools-macos-arm64-100.10.0/bin/mongodump'        # Update with the path to your mongodump tool
-MONGORESTORE_PATH = '/Users/rahul.verma/Desktop/vCore Compatibility Test Suite/mongodb-database-tools-macos-arm64-100.10.0/bin/mongorestore'  # Update with the path to your mongorestore tool
+MONGODUMP_PATH = ''        # Update with the path to your mongodump tool
+MONGORESTORE_PATH = ''  # Update with the path to your mongorestore tool
